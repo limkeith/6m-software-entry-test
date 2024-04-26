adb7ed6583b9f04c3e93289fc9d84a369215b581
limkeith <lim_keith@yahoo.com.sg>
2024-04-27 02:23:13 +0800

    q7

--- src/q7.js
+++ src/q7.js
@@ -14,7 +14,10 @@ The form of this function is using the arrow function format. It allows for shor
 
 Task 1: Modify this function to use object destructuring for the inputs to display only the first name and hobby properties of the object to the console:
 */
-const introduce = () => {
+
+const greet = (person) => {
+  const { firstName, hobby } = person;
+  console.log(`Hello, my name is ${firstName} and I love ${hobby}!`);
 }
 
 greet(person);
